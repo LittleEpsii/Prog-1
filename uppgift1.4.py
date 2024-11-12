4067aedfce67c413ad7b953e127f263dc06e6dcd
LittleEpsii <littleepsii@gmail.com>
2024-11-12 10:52:19 +0100

    Add files via upload

---
+++ uppgift1.4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import math
+
+x=2.0
+
+y = (math.exp(math.sin(2*math.pi)) + math.log(1/x) * math.cos(math.pi/2*x)**4 + math.sqrt(3*x))
+
+y = round(y,3)
+
+print("Resultatet av beräkningen :",y)
+
